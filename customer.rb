1a54053daff1a75f39b97a02794b8b5e2ecf6ee9
daniel kelly <danieljkelly91@gmail.com>
2018-01-31 11:42:51 +0000

    Adds test and function buy drink and reduce wallet

--- customer.rb
+++ customer.rb
@@ -5,6 +5,18 @@ class Customer
   def initialize(name, wallet)
     @name = name
     @wallet = wallet
+    @number_of_drinks = []
   end
+
+  def buy_drink(drink)
+    @number_of_drinks << drink
+    @wallet -= drink.price()
+  end
+
+  def number_of_drinks()
+    @number_of_drinks.length()
+  end
+
+
 
 end
--- specs/customer_spec.rb
+++ specs/customer_spec.rb
@@ -1,6 +1,8 @@
 require('minitest/rg')
 require('minitest/autorun')
 require_relative('../customer.rb')
+require_relative('../drink.rb')
+
 
 class CustomerTest < MiniTest::Test
 
@@ -13,7 +15,22 @@ class CustomerTest < MiniTest::Test
   end
 
   def test_wallet_has_funds
-    assert_equal(100.00, @customer.wallet)    
+    assert_equal(100.00, @customer.wallet)
   end
+
+  def test_customer_can_buy_drink
+    drink = Drink.new("beer", 2.99)
+    @customer.buy_drink(drink)
+    assert_equal(1, @customer.number_of_drinks())
+  end
+
+  def test_wallet_reduces_when_buys_a_drink
+    drink = Drink.new("beer", 2.99)
+    @customer.buy_drink(drink)
+    
+    assert_equal(97.01, @customer.wallet)
+
+  end
+
 
 end
